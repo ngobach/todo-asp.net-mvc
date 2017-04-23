21c9ff15d1f80bcd72846a6fa863cbdc39cb4c1e
Ngô Xuân Bách <thanbaiks@gmail.com>
2017-04-23 16:34:59 +0700

    Update site

---
+++ MvcTodo/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTodo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult MyView()
+        {
+            return View();
+        }
+    }
+}
